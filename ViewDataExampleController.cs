01751615c1eb4ce9059b92c76f5260d86d5eac47
karravularajeswari <43335512+karravularajeswari@users.noreply.github.com>
2020-04-27 14:23:59 +0530

    Add files via upload

---
+++ ViewDataExampleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPractice.Controllers
+{
+    public class ViewDataExampleController : Controller
+    {
+        // GET: ViewDataExample
+        public ActionResult Index()
+        {
+            ViewData.Add("uname", "Raje");
+            /*ViewData["Uname"] = "Prem";*/
+            ViewData["Categories"] = new List<String>()
+            {
+                "Electronics",
+                "shoes"
+            };
+            return View();
+        }
+    }
+}
